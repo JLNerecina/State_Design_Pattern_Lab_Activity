d95b59d23cf48eb78491d30ec65a4626a38a85c5
John Lian R. Nerecina <johnliannerecina@gmail.com>
2026-02-10 01:07:48 +0000

    Status: Completed Programmer: John Lian Nerecina Date: February 10, 2026

---
+++ Account.java
@@ -0,0 +1,60 @@
+public class Account{
+    private String accountNumber;
+    private double balance;
+    private AccountState accountState;
+
+    public Account(String accountNumber, double balance){
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.accountState = new ActiveState();
+    }
+
+    public void deposit(double depositAmount){
+        accountState.deposit(this, depositAmount);
+    }
+
+    public void withdraw(double withdrawAmount){
+        accountState.withdraw(this, withdrawAmount);
+    }
+
+    public void suspend(){
+        accountState.suspend(this);
+    }
+
+    public void activate(){
+        accountState.activate(this);
+    }
+
+    public void close(){
+        accountState.close(this);
+    }
+
+    @Override
+    public String toString(){
+        return "Account {" + "accountNumber = '" + accountNumber + '\'' + ", balance = " + balance + '}';
+    }
+    
+    public String getAccountNumber(){
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber){
+        this.accountNumber = accountNumber;
+    }
+
+    public double getBalance(){
+        return balance;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    public AccountState getAccountState(){
+        return accountState;
+    }
+
+    public void setAccountState(AccountState accountState){
+        this.accountState = accountState;
+    }
+}
